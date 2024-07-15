6b6c0238a1707901d2f440b7144c93a1129e5e1e
Aryan Saxena <aryansaxenaalig@gmail.com>
2024-07-15 17:24:17 +0530

    update feature

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,9 +1,9 @@
 <script>
-    export let names
+	export let names;
 </script>
 
 <div
-	class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
+	class="mt-10 pt-10 w-full max-w-4xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
 >
 	<div class="flex items-center justify-between mb-4">
 		<div class="space-y-1">
@@ -19,15 +19,21 @@
 				<div class="flex items-center space-x-4">
 					<div class="flex">
 						<p class="font-medium pt-1 leading-none">{user.name}</p>
-						<p class="font-medium pl-5 text-gray-500 pt-0">{user.email}</p>                        
+						<p class="font-medium pl-5 text-gray-500 pt-0">
+							{user.email}
+						</p>
 					</div>
 				</div>
-                <form method="POST" action="/profiles?/delete">
-                <input type="hidden" name="id" id="id" value={user.id}>
-                <button type="submit">
-                    <img class="w-4 float-right" src="./trash-can.svg" alt="delete"/>
-                </button>
-                </form>
+				<form method="POST" action="/profiles?/delete">
+					<input type="hidden" name="id" id="id" value={user.id} />
+					<button type="submit">
+						<img
+							class="w-4 float-right"
+							src="./trash-can.svg"
+							alt="delete"
+						/>
+					</button>
+				</form>
 				<form method="POST" action="/profiles?/update">
 					<input type="hidden" name="id" id="id" value={user.id} />
 					<input
@@ -50,6 +56,7 @@
 					>
 						Update
 					</button>
+				</form>
 			</div>
 		{/each}
 	</div>
